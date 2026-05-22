d3d10df613ad270ae522cba042e392502a0679c9
MacBook <macbook@MacBook-Air-od-MacBook.local>
2026-05-22 19:33:43 +0300

    Lab 7 solution by student 18

---
+++ lab7/solution/student-18/philosophers.c
@@ -0,0 +1,211 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+typedef enum e_state
+{
+    THINKING,
+    HUNGRY,
+    EATING
+}   t_state;
+
+// enum для отслеживания вилок в руках
+typedef enum e_philo_flags
+{
+    PHILO_NONE      = 0,
+    PHILO_HAS_LEFT  = 1,
+    PHILO_HAS_RIGHT = 2
+}   t_philo_flags;
+
+typedef struct s_philosopher
+{
+    int             id;
+    t_state         state;
+    t_philo_flags   flags;
+    pthread_t       thread;
+    pthread_mutex_t *left_fork;
+    pthread_mutex_t *right_fork;
+    int             meals_eaten;
+    int             max_meals;
+}   t_philosopher;
+
+// мьютекс для вывода в консоль
+static pthread_mutex_t print_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+static void safe_print(t_philosopher *philo, const char *msg)
+{
+    pthread_mutex_lock(&print_mutex);
+    printf("Philosopher %d %s\n", philo->id, msg);
+    pthread_mutex_unlock(&print_mutex);
+}
+
+// философ думает
+static void thinking(t_philosopher *philo)
+{
+    if (philo->state != THINKING)
+    {
+        philo->state = THINKING;
+        philo->flags = PHILO_NONE;
+        safe_print(philo, "is THINKING");
+    }
+    usleep(80000);
+}
+
+// кушает
+static void eating(t_philosopher *philo)
+{
+    if (philo->state != EATING)
+    {
+        philo->state = EATING;
+        safe_print(philo, "is EATING");
+        philo->meals_eaten++;
+    }
+    usleep(80000);
+}
+
+// захват вилок без deadlock
+
+static void take_forks(t_philosopher *philo)
+{
+    if (philo->state == HUNGRY)
+        return;
+    
+    philo->state = HUNGRY;
+    safe_print(philo, "is HUNGRY");
+    
+    /* Четные (2,4) берут сначала левую, потом правую
+    Нечетные (1,3,5) берут сначала правую, потом левую */
+
+    if (philo->id % 2 == 0)
+    {
+        pthread_mutex_lock(philo->left_fork);
+        philo->flags |= PHILO_HAS_LEFT;
+        safe_print(philo, "took left fork");
+        
+        pthread_mutex_lock(philo->right_fork);
+        philo->flags |= PHILO_HAS_RIGHT;
+        safe_print(philo, "took right fork");
+    }
+    else
+    {
+        pthread_mutex_lock(philo->right_fork);
+        philo->flags |= PHILO_HAS_RIGHT;
+        safe_print(philo, "took right fork");
+        
+        pthread_mutex_lock(philo->left_fork);
+        philo->flags |= PHILO_HAS_LEFT;
+        safe_print(philo, "took left fork");
+    }
+}
+
+// освобождение вилок
+static void put_forks(t_philosopher *philo)
+{
+    if (philo->flags & PHILO_HAS_LEFT)
+    {
+        pthread_mutex_unlock(philo->left_fork);
+        philo->flags &= ~PHILO_HAS_LEFT;
+    }
+    
+    if (philo->flags & PHILO_HAS_RIGHT)
+    {
+        pthread_mutex_unlock(philo->right_fork);
+        philo->flags &= ~PHILO_HAS_RIGHT;
+    }
+    
+    if (philo->flags == PHILO_NONE)
+        safe_print(philo, "put both forks");
+}
+
+// Основной цикл философа
+static void *philosopher_routine(void *arg)
+{
+    t_philosopher *philo = (t_philosopher *)arg;
+    
+    while (philo->meals_eaten < philo->max_meals)
+    {
+        thinking(philo);
+        take_forks(philo);
+        eating(philo);
+        put_forks(philo);
+    }
+    
+    safe_print(philo, "FINISHED eating");
+    return (NULL);
+}
+
+int main(void)
+{
+    pthread_mutex_t     forks[5];
+    t_philosopher       philosophers[5];
+    int                 i;
+    int                 num_philosophers = 5;
+    int                 max_meals = 3;
+    
+    printf("========================================\n");
+    printf("Dining Philosophers Problem\n");
+    printf("========================================\n");
+    printf("Philosophers: %d\n", num_philosophers);
+    printf("Each will eat: %d time(s)\n", max_meals);
+    printf("========================================\n\n");
+    
+    // инициализация мьютексов (вилок)
+    i = 0;
+    while (i < num_philosophers)
+    {
+        pthread_mutex_init(&forks[i], NULL);
+        i++;
+    }
+    
+    //инициализация философов 
+    i = 0;
+    while (i < num_philosophers)
+    {
+        philosophers[i].id = i + 1;
+        philosophers[i].state = THINKING;
+        philosophers[i].flags = PHILO_NONE;
+        philosophers[i].left_fork = &forks[i];
+        philosophers[i].right_fork = &forks[(i + 1) % num_philosophers];
+        philosophers[i].meals_eaten = 0;
+        philosophers[i].max_meals = max_meals;
+        i++;
+    }
+    
+    // потоки философов
+    i = 0;
+    while (i < num_philosophers)
+    {
+        pthread_create(&philosophers[i].thread, NULL, 
+                      philosopher_routine, &philosophers[i]);
+        i++;
+    }
+    
+    // Ожидание завершения всех философов
+    i = 0;
+    while (i < num_philosophers)
+    {
+        pthread_join(philosophers[i].thread, NULL);
+        i++;
+    }
+    
+    printf("\n========================================\n");
+    i = 0;
+    while (i < num_philosophers)
+    {
+        printf("Philosopher %d ate %d time(s)\n", 
+               philosophers[i].id, philosophers[i].meals_eaten);
+        i++;
+    }
+    printf("========================================\n");
+    
+    // уничтожение мьютексов
+    i = 0;
+    while (i < num_philosophers)
+    {
+        pthread_mutex_destroy(&forks[i]);
+        i++;
+    }
+    
+    return (0);
+}
